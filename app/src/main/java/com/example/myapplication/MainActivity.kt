36f45226b32fae9fe1f42ab43956168bb0b5e86b
rieadaits <rieadaits21@gmail.com>
2023-05-07 16:30:11 +0600

    working

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,20 +4,26 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.*
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -25,7 +31,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -76,8 +81,21 @@ fun MessageCard(name: String, body: String) {
                 )
                 Spacer(modifier = Modifier.width(8.dp))
                 Column(modifier = Modifier.padding(horizontal = 8.dp, vertical = 1.dp)) {
-                    Text(name, style = TextStyle(color = Color.Cyan))
-                    Text(body, color = Color.Blue)
+                    Text(
+                        name, color = Color.Red,
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Surface(
+                        shape = MaterialTheme.shapes.medium,
+                        shadowElevation = 3.dp,
+                    ) {
+                        Text(
+                            body, color = Color.Blue,
+                            modifier = Modifier.padding(all = 4.dp),
+                            style = MaterialTheme.typography.bodyMedium,
+                        )
+                    }
                 }
             }
 
@@ -96,8 +114,10 @@ fun MessageCard(name: String, body: String) {
 
 
         }
-        LazyColumn(modifier = Modifier
-            .padding(8.dp)) {
+        LazyColumn(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
             items(100) { index ->
                 Text(text = "${index + 1}",
                     modifier = Modifier.clickable { println(index) })
