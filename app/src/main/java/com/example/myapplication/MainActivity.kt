e1e38849e3a5b2a7d1b45d25a0bf71b010939c10
rieadaits <rieadaits21@gmail.com>
2023-05-09 12:18:14 +0600

    expanded animation added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -24,17 +26,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +47,6 @@ class MainActivity : ComponentActivity() {
             ) {
                 MessageCard(
                     "Hey there!",
-                    "how are you",
                 )
             }
 
@@ -55,88 +56,109 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MessageCard(name: String, body: String) {
+fun MessageCard(name: String) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Gray),
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-            Row(modifier = Modifier.padding(8.dp)) {
-                Image(
-                    painter = painterResource(id = R.drawable.icon),
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(shape = CircleShape)
-                        .border(
-                            width = 2.dp,
-                            color = MaterialTheme.colorScheme.primary,
-                            shape = CircleShape
-                        ),
-                    contentDescription = "sample icon"
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Column(modifier = Modifier.padding(horizontal = 8.dp, vertical = 1.dp)) {
-                    Text(
-                        name, color = Color.Red,
-                        style = MaterialTheme.typography.titleSmall
-                    )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Surface(
-                        shape = MaterialTheme.shapes.medium,
-                        shadowElevation = 3.dp,
-                    ) {
-                        Text(
-                            body, color = Color.Blue,
-                            modifier = Modifier.padding(all = 4.dp),
-                            style = MaterialTheme.typography.bodyMedium,
-                        )
-                    }
-                }
-            }
 
-
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data("https://via.placeholder.com/600/92c952").crossfade(true).build(),
-                placeholder = painterResource(R.drawable.ic_launcher_background),
-                contentDescription = "test",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(70.dp)
-                    .padding(8.dp)
-                    .clip(shape = CircleShape)
-            )
-
-
-        }
         LazyColumn(
             modifier = Modifier
                 .padding(8.dp)
         ) {
             items(100) { index ->
-                Text(text = "${index + 1}",
-                    modifier = Modifier.clickable { println(index) })
+                MessageCardTile(name, (index + 1).toString())
             }
+
         }
 
     }
 
 }
 
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
-
-@Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard("Hey there!", "how are you")
+@Preview
+fun MessageCardTile(name: String, body: String) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+
+
+        Row(
+            modifier = Modifier
+                .padding(all = 8.dp)
+                .fillMaxWidth()
+                .background(Color.Gray),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.icon),
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(shape = CircleShape)
+                    .border(
+                        width = 2.dp,
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = CircleShape
+                    ),
+                contentDescription = "sample icon"
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            // We keep track if the message is expanded or not in this
+            // variable
+            var isExpanded by remember { mutableStateOf(false) }
+            // surfaceColor will be updated gradually from one color to the other
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            )
+
+            // We toggle the isExpanded variable when we click on this Column
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Text(
+                    text = name,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleSmall
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                    // surfaceColor color will be changing gradually from primary to surface
+                    color = surfaceColor,
+                    // animateContentSize will change the Surface size gradually
+                    modifier = Modifier
+                        .animateContentSize()
+                        .padding(horizontal = 1.dp, vertical = 4.dp)
+                ) {
+                    Text(
+                        text = body + "long text,long text,long text,long text,long text,long text,long text,long text,long text,long text,long text,long text,",
+                        modifier = Modifier.padding(all = 8.dp),
+                        // If the message is expanded, we display all its content
+                        // otherwise we only display the first line
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+
+        @Preview(name = "Light Mode")
+        @Preview(
+            uiMode = Configuration.UI_MODE_NIGHT_YES,
+            showBackground = true,
+            name = "Dark Mode"
+        )
+
+        @Preview
+        @Composable
+        fun PreviewMessageCard() {
+            MessageCard("Hey there!")
+        }
+    }
 }
